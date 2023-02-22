5c75c779ea62ea251c685ab428b902c7ebe3a2a7
sikandarjavid-786 <sjaved@eescorporation.com>
2023-02-22 20:53:56 +0500

    first commint

---
+++ Monster Rolodex/src/components/card-list/card-list.component.tsx
@@ -0,0 +1,17 @@
+import Card from '../card/card.component';
+import { Monster } from '../../App';
+import './card-list.styles.css';
+
+type CardListProps = {
+  monsters: Monster[];
+}
+
+const CardList = ({ monsters }: CardListProps) => (
+  <div className='card-list'>
+    {monsters.map((monster) => {
+      return <Card key={monster.id} monster={monster} />;
+    })}
+  </div>
+);
+
+export default CardList;
